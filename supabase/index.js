ca1e406fa9a152db767a58fa623809b861fe6272
kazuma_0 <as8644459@gmail.com>
2022-04-17 11:28:05 +0530

    Added methods for supabase

--- supabase/index.js
+++ supabase/index.js
@@ -1,23 +1,21 @@
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const client = createClient(
-  "https://idsrylwzfaptxhtwytfw.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlkc3J5bHd6ZmFwdHhodHd5dGZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk5MzkyNjksImV4cCI6MTk2NTUxNTI2OX0.vCSpo2uhmi5_WAsMqsy1SHkQQhg3h0PsLOqpk6DgcNc",
+  '',
+  '',
   {
     autoRefreshToken: true,
   }
 )
-const login = async (email, password)=>{
+export const login = async (email) => {
+  console.log(email)
   return await client.auth.signIn({
     email,
-    password,
   })
 }
-const getUser = () =>{
+export const getUser = () => {
   return client.auth.user()
 }
-export {
-  login,
-  getUser
+export const logOut = _ =>{
+  return client.auth.signOut()
 }
-
