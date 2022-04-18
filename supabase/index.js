c4a059ab1ec56585e5f4b0dc6c5b5d502f89f800
kaz <as8644459@gmail.com>
2022-04-18 11:19:57 +0530

    add public key and token
    will be reset soon

--- supabase/index.js
+++ supabase/index.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const client = createClient(
-  '',
-  '',
+  'https://idsrylwzfaptxhtwytfw.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlkc3J5bHd6ZmFwdHhodHd5dGZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk5MzkyNjksImV4cCI6MTk2NTUxNTI2OX0.vCSpo2uhmi5_WAsMqsy1SHkQQhg3h0PsLOqpk6DgcNc',
   {
     autoRefreshToken: true,
   }
